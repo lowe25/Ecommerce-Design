9d3a13083808c9200335397c850d8ff83f6adf6a
Louie Cabral <louiealdrin25@gmail.com>
2021-07-13 18:22:52 +0800

    Column Button Added!

--- js/nav.js
+++ js/nav.js
@@ -1,6 +1,20 @@
-const toggleButton = document.getElementsByClassName('nav-burger')[0]
-const navbarLinks = document.getElementsByClassName('nav-list')[0]
+const toggleButton = document.getElementsByClassName("nav-burger")[0];
+const navbarLinks = document.getElementsByClassName("nav-list")[0];
 
-toggleButton.addEventListener('click', () => {
-  navbarLinks.classList.toggle('active')
-})
+toggleButton.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active");
+});
+
+const col = document.querySelector(".col").addEventListener("click", col1);
+const row = document.querySelector(".row").addEventListener("click", row1);
+const div = document.querySelector(".product-landing-showcase");
+const div1 = document.querySelector(".motherboard-showcase");
+
+
+function col1() {
+  div.style.display = "grid";
+}
+function row1() {
+  div.style.display = "flex";
+  div.style.flexDirection = "column";
+}
